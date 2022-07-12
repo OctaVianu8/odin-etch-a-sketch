ae1cb46a44a5dc315e688a6b6dd74bc5d43dd720
Stanescu Octavian <stanescu.matei.octavian@gmail.com>
2022-07-12 16:34:39 +0300

    add color option and monochrome toggle

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const canvasSize = 960;
 const canvas = document.querySelector(".container")
 const changeResBtn = document.querySelector('.change-res-btn')
-const currResText = document.querySelector('.curr-res');
+const currResText = document.querySelector('.curr-res')
+const monochromeBtn = document.querySelector('.monochrome-btn')
+let monochrome = false
+const canvasSize = 960
 
-changeResBtn.addEventListener('click', changeRes);
+changeResBtn.addEventListener('click', changeRes)
+monochromeBtn.addEventListener('click', monochromeToggle)
 
 function changeRes() {
     res = prompt('What would you like the canvas resolution to be? (A.K.A. number of boxes per row)');
@@ -50,16 +53,27 @@ function addDivs(divPerRow) {
 }
 
 function colorDiv(e) {
-    
-    opacity = (Math.floor((--opacityArr[+e.target.id])/10*256)).toString(16);
-    //console.log(opacityArr[+e.target.id]);
-    randomColor = getRandomColor()
+    let divOpacity = --opacityArr[+e.target.id];
+    opacity = (divOpacity >= 0) ? Math.floor(divOpacity/10*256).toString(16) : '00'
+    console.log(opacity);
+    randomColor = (monochrome) ? '#ffffff' : getRandomColor()
     e.target.style.backgroundColor = randomColor + opacity;
 }
 
 function getRandomColor()
 {
-    return '#ffffff';
+    let red = Math.floor(Math.random()*256);
+    let green = Math.floor(Math.random()*256);
+    let blue = Math.floor(Math.random()*256);
+    let result = '#' + red.toString(16) + green.toString(16) + blue.toString(16);
+    console.log(result);
+    return result;
+}
+
+function monochromeToggle()
+{
+    monochrome = !monochrome;
+    monochromeBtn.textContent = (monochrome) ? 'Monochrome: On' : 'Monochrome: Off'
 }
 
 addDivs(32);
